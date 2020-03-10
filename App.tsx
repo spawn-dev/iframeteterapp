4fa2ff6c4e49e1fadb51942566b46b83ba5dfdf7
spawn-dev <60055172+spawn-dev@users.noreply.github.com>
2020-03-10 09:45:27 -0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import {WebView} from 'react-native-webview';
 export default function App() {
   return (
     <WebView 
-    source={{uri: 'https://www.figma.com/proto/QkpEf1Bg5hfmc00T0jSboN/Spawn?node-id=19%3A2&scaling=scale-down'}}
+    source={{uri: 'https://www.figma.com/proto/WRbH56ldcnYY0c7ks4g4FH/Monumental-Construction?node-id=7%3A2&scaling=scale-down'}}
     style={{marginTop: 20}}
   />
   );
